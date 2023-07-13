6c2be9cfae6dbb81edc259799d04f2914d5b2bfe
Wonders_El369 <131933965+odzorp@users.noreply.github.com>
2023-07-13 21:13:14 +0100

    Add files via upload

---
+++ README.md
@@ -0,0 +1,61 @@
+# To-Do App
+
+This is a simple To-Do app that allows users to manage their tasks. The app provides a user-friendly interface with various features to enhance productivity. In addition to the basic task management functions such as adding, completing, and deleting tasks, the app also supports drag and drop functionality to enable users to reorder their to-do items.
+
+## Features
+
+### 1. Optimal Layout 
+
+The app is designed to provide an optimal layout based on the user's device screen size.
+
+### 2. Toggle Light and Dark Mode
+
+Users can switch between light and dark modes to customize their app experience.
+
+### 3. Hover States for Interactive Elements
+
+All interactive elements in the app have hover states to provide visual feedback to users.
+
+### 4. Add New To-Dos
+
+Users can easily add new to-dos to their list.
+
+### 5. Mark To-Dos as Complete
+
+Users can mark their to-dos as complete .
+
+### 6. Delete To-Dos
+
+ Users can permanently delete completed tasks from the list.
+
+### 7. Filter To-Dos
+
+The app provides filtering options to help users organize their tasks. Users can choose to view all tasks, only active tasks, or only completed tasks. 
+
+### 8. Clear All Completed To-Dos
+
+For convenience, the app provides a 'clear all completed' button at the bottom of the task list. Clicking this button will permanently remove all completed tasks from the list.
+
+### 9. Drag and Drop to Reorder Items
+
+The app should have supported drag and drop functionality, enabling users to reorder their to-do items. Users can click and hold on a to-do item, then drag it to a new position on the list. Once the desired position is reached, users can release the mouse button to drop the item in its new location but i could not complete that.
+
+## Usage
+
+1. Clone the repository or download the project files.
+2. Open the `index.html` file in a web browser.
+3. Use the app to manage your to-do list by following the provided functionalities described above.
+4. Enjoy a productive and organized experience!
+
+## Technologies Used
+
+The To-Do app is built as a full-stack using the following technologies:
+
+- HTML
+- CSS
+- JavaScript (Vanilla)
+
+## Credits
+
+This app was created by [PILILP ODZOR]. If you have any questions or suggestions, please feel free to contact me at [odzorp3@gmail.com/philip.odzor@amalitech.org].
+ Feel free to modify and use the code according to your needs.
---
+++ script.js
@@ -0,0 +1,276 @@
+/******************************/
+/***   GLOBAL VARIABLES     ***/
+/******************************/
+
+let todoArray = [];
+let todoId = 0;
+let currentFilter = 'all';
+const LOCAL_TODOS = "local_todos";
+
+const todoInput = document.getElementById("todo-input");
+const todoList = document.getElementById("todo-list");
+const itemsLeft = document.getElementById("items-left");
+const todoFilters = document.querySelectorAll("input[name='filter']");
+const btnClear = document.getElementById('clear-completed');
+
+const themeSwitch = document.getElementById('theme-toggle');
+const themeLogos = document.querySelectorAll('.btn-theme img');
+
+/******************************/
+/***  EVENT LISTENERS       ***/
+/******************************/
+
+btnClear.addEventListener('click', () => {
+   const toRemove = todoArray.filter((obj) => obj.active === false);
+
+   if (toRemove.length > 0 &&  confirm(`You are about to remove ${toRemove.length} completed task(s). Are you sure?`)) {
+      toRemove.forEach((elem) => {
+         removeElem(elem.DOMelem);
+      });
+   }
+});
+
+themeSwitch.addEventListener('click', themeSwitcher);
+
+todoInput.addEventListener("keyup", (e) => {
+   if (e.key === "Enter") {
+      if (e.target.value !== "") {
+         addTodoElem(e.target.value);
+         todoInput.value = "";
+         refreshFilters();
+      } else {
+         alert("You must write something before adding an item.");
+      }
+   }
+});
+
+todoFilters.forEach((filter) => {
+   filter.addEventListener('change', filterCallback);
+});
+
+/************************************/
+/***  Event Listeners and Callbacks ***/
+/************************************/
+
+function themeSwitcher(e) {
+   themeLogos.forEach(logo => logo.classList.toggle("todo-elem-hide"));
+
+   if (!document.body.dataset.theme) {
+      document.body.dataset.theme = "darkTheme";
+   } else {
+      document.body.dataset.theme = "";
+   }
+}
+
+function filterCallback(e) {
+   currentFilter = e.target.value;
+   refreshFilters();
+}
+
+function refreshFilters() {
+   if (currentFilter === 'completed') {
+      completedCB();
+   } else if (currentFilter === 'all') {
+      allCB();
+   } else { // if active
+      activeCB();
+   }
+}
+
+function completedCB() {
+   todoArray.forEach(function (arrayObj)  {
+      if (!arrayObj.active && arrayObj.DOMelem.classList.contains("todo-elem-hide")) {
+         arrayObj.DOMelem.classList.remove("todo-elem-hide");
+      } else if (arrayObj.active && !arrayObj.DOMelem.classList.contains("todo-elem-hide")) {
+         arrayObj.DOMelem.classList.add("todo-elem-hide");      
+      }
+   });
+}
+
+function allCB() {
+   todoArray.forEach(function (arrayObj)  {
+      if (arrayObj.DOMelem.classList.contains("todo-elem-hide")) {
+         arrayObj.DOMelem.classList.remove("todo-elem-hide");
+      }
+   });
+}
+
+function activeCB() {
+   todoArray.forEach(function (arrayObj)  {
+      if (arrayObj.active && arrayObj.DOMelem.classList.contains("todo-elem-hide")) {
+         arrayObj.DOMelem.classList.remove("todo-elem-hide");
+      } else if (arrayObj.active === false && !arrayObj.DOMelem.classList.contains("todo-elem-hide")) {
+         arrayObj.DOMelem.classList.add("todo-elem-hide");
+      }
+   });
+}
+
+/******************************/
+/***  FUNCTIONS             ***/
+/******************************/
+
+/**  MISCELLANEOUS  FUNCTIONS**/
+/******************************/
+
+function updateActiveCount() {
+   let count = todoArray.reduce((count, todoObj) => {
+      if (todoObj.active) count++;
+      return count;
+   }, 0);
+   itemsLeft.innerText = count;
+}
+
+function updateCurrentId() {
+   if (!todoArray.length) {
+      todoId = 0;
+   } else {
+      todoId = todoArray[todoArray.length - 1].id + 1;
+   }
+}
+
+/**  localStorage FUNCTIONS **/
+/******************************/
+
+function getLocalStorage() {
+   if (localStorage.getItem(LOCAL_TODOS) === null) {
+      localStorage.setItem(LOCAL_TODOS, JSON.stringify([]));
+   } else if (JSON.parse(localStorage.getItem(LOCAL_TODOS)).length) {
+      todoArray = JSON.parse(localStorage.getItem(LOCAL_TODOS));
+      todoArray.forEach((todoElem) => {
+         if (todoId < +todoElem.id) todoId = +todoElem.id;
+         addTodoElem(todoElem.content, false);
+      });
+      todoId++;
+   }
+   updateActiveCount();
+}
+
+function updateLocalStorage() {
+   localStorage.setItem(LOCAL_TODOS, JSON.stringify(todoArray));
+}
+
+function removeFromStorage(id) {
+   todoArray = todoArray.filter((todoObj) => {
+      return todoObj.id !== +id;
+   });
+
+   updateLocalStorage();
+}
+
+/**  DOM EFFECT FUNCTIONS    **/
+/******************************/
+
+function changeActiveStatus(elem) {
+   elem.classList.toggle("todo-elem-checked");
+   let isActive = true;
+
+   if (elem.classList.contains("todo-elem-checked")) {
+      isActive = false;
+   }
+
+   todoArray.forEach((arrayObj) => {
+      if (arrayObj.id === +elem.id) arrayObj.active = isActive;
+   });
+
+   updateLocalStorage();
+   updateActiveCount();
+}
+
+function removeElem(element) {
+   removeElemFromDom(element);
+   removeFromStorage(+element.id);
+   updateCurrentId();
+   updateActiveCount();
+   refreshFilters();
+}
+
+function removeElemFromDom(elem) {
+   elem.remove();
+}
+
+function addTodoElem(todoText, isNew = true) {
+   const todoEl = document.createElement("li");
+   todoEl.classList.add("todo-elem");
+   todoEl.id = "" + todoId;
+
+   const todoCheck = document.createElement("button");
+   todoCheck.classList.add("btn", "todo-check");
+   const checkIcon = document.createElement("img");
+   checkIcon.src = "images/icon-check.svg";
+   checkIcon.alt = "Check";
+   todoCheck.appendChild(checkIcon);
+   todoEl.appendChild(todoCheck);
+
+   const todoTextEl = document.createElement("p");
+   todoTextEl.textContent = todoText;
+   todoEl.appendChild(todoTextEl);
+
+   const todoDelete = document.createElement("button");
+   todoDelete.classList.add("btn", "todo-delete");
+   const deleteIcon = document.createElement("img");
+   deleteIcon.src = "images/icon-cross.svg";
+   deleteIcon.alt = "Delete";
+   todoDelete.appendChild(deleteIcon);
+   todoEl.appendChild(todoDelete);
+
+   if (isNew) {
+      todoArray.unshift({
+         active: true,
+         content: todoText,
+         DOMelem: todoEl,
+         id: todoId++,
+      });
+      updateLocalStorage();
+   } else {
+      todoArray.forEach((arrayObj) => {
+         if (arrayObj.id === todoId) {
+            arrayObj.DOMelem = todoEl;
+            if (!arrayObj.active) {
+               todoEl.classList.add("todo-elem-checked");
+            }
+         }
+      });
+   }
+
+   todoList.insertBefore(todoEl, todoList.firstChild);
+
+   todoCheck.addEventListener("click", function() {
+      changeActiveStatus(todoEl);
+      refreshFilters();
+   });
+
+   todoDelete.addEventListener("click", function() {
+      removeElem(todoEl);
+   });
+
+   updateActiveCount();
+}
+
+/***  START OF INSTRUCTIONS     ***/
+/**********************************/
+
+function init() {
+   const starterList = [
+      "Complete online JavaScript course",
+      "Jog around the park 3x",
+      "10 minutes meditation",
+      "Read for 1 hour",
+      "Pick up groceries",
+   ];
+
+   if (localStorage.getItem("isFirstVisit") === null || localStorage.getItem("isFirstVisit") === false) {
+      localStorage.setItem("isFirstVisit", true);
+      starterList.forEach((item, index) => {
+         addTodoElem(item);
+         if (index === 0) {
+            const firstItem = todoList.firstChild;
+            changeActiveStatus(firstItem);
+         }
+      });
+   } else {
+      getLocalStorage();
+   }
+}
+
+
+init();
